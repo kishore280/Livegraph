1194b17bad51fa53e2caea006b99f7219f57e216
kishore280 <maheshwarankishore@gmail.com>
2026-09-14 14:02:45 +0530

    thodakkammm

---
+++ backend/server/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from server.routers import system_router
+
+name = "LiveGraph Backend"
+version = "0.1.0"
+app =  FastAPI(title= name, version= version)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+app.include_router(system_router.router)
+
---
+++ backend/server/routers/system_router.py
@@ -0,0 +1,11 @@
+
+from fastapi import APIRouter
+
+
+
+router = APIRouter(prefix="/api/system", tags=["system"])
+
+@router.get("/health")
+async def health():
+    return {"status": "alive"}
+
