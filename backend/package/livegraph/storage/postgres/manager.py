777c2f880636eba501e4f252986e2b360c399eb7
kishore280 <maheshwarankishore@gmail.com>
2026-09-17 22:22:47 +0530

    Bump business schema version to 3

--- backend/package/livegraph/storage/postgres/manager.py
+++ backend/package/livegraph/storage/postgres/manager.py
@@ -6,7 +6,7 @@ from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_asyn
 from livegraph.storage.postgres.models import Base
 
 SCHEMA_VERSION_TABLE = "livegraph_schema_migrations"
-BUSINESS_SCHEMA_VERSION = 2
+BUSINESS_SCHEMA_VERSION = 3
 
 
 class PostgresManager:
